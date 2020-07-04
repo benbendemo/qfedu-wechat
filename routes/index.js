c5af7a149b610436ccd30e54c4e13fa666a3d96c
benbendemo <xc0910@hotmail.com>
2020-07-04 14:46:03 +0800

    [feature] auth

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var sha1 = require('sha1');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,18 @@ router.get('/', function(req, res, next) {
 
 /* 微信鉴权接口 */
 router.get('/auth', function(req, res) {
-  res.send('wechat authentication interface')
+    let {signature, timestamp, nonce, echostr} = req.query;
+    let token = 'wechatwebapp20191215';
+    let array = [timestamp, nonce, token];
+    array.sort();
+    let newStr = array.join('');
+    let resultStr = sha1(newStr);
+    if (resultStr === signature){
+        res.set('Content-Type', 'text/plain')
+        res.send(echostr)
+    }else{
+        res.send('auth error')
+    }
 });
 
 module.exports = router;
